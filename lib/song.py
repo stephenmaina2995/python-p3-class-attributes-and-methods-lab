9fe6e3f93b7dc9f8a034fabda95dd2b8d21b6745
stephenmaina2995 <stephenmaina2995@gmail.com>
2023-05-18 15:04:33 +0300

    Completed assignment

--- lib/song.py
+++ lib/song.py
@@ -1,5 +1,28 @@
 class Song:
-    def __init__(self, name,artist,genre):
-        self.name=name
-        self.artist=artist
-        self.genre=genre
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.count += 1
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        if self.genre not in Song.genre_count:
+            Song.genre_count[self.genre] = 1
+        else:
+            Song.genre_count[self.genre] += 1
